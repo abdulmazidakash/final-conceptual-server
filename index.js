936a02d343783fec2001f1f7be8927162f7aea68
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-01-02 09:06:55 +0600

    first commit

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-const express = require('express');
-const cors = require('cors');
 require('dotenv').config()
-const app = express();
-const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const jwt = require('jsonwebtoken')
+const morgan = require('morgan')
 
-//middleware
-app.use(cors());
-app.use(express.json());
+const port = process.env.PORT || 9000
+const app = express()
+// middleware
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 
+app.use(express.json())
+app.use(cookieParser())
+app.use(morgan('dev'))
 
-//
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token
 
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err)
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.user = decoded
+    next()
+  })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j0hxo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -21,33 +44,54 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
-});
-
+  },
+})
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-
-
+    // Generate jwt token
+    app.post('/jwt', async (req, res) => {
+      const email = req.body
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '365d',
+      })
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true })
+    })
+    // Logout
+    app.get('/logout', async (req, res) => {
+      try {
+        res
+          .clearCookie('token', {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          })
+          .send({ success: true })
+      } catch (err) {
+        res.status(500).send(err)
+      }
+    })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
-run().catch(console.dir);
+run().catch(console.dir)
 
-
-
-//
-app.get('/', (req, res)=>{
-	res.send('final conceptual session server is run')
+app.get('/', (req, res) => {
+  res.send('Hello from plantNet Server..')
 })
 
-app.listen(port, ()=>{
-	console.log(`server is running port: ${port}`);
+app.listen(port, () => {
+  console.log(`plantNet is running on port ${port}`)
 })
