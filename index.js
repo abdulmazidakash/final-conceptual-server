c9a5dc30eb3b667f814848435c139a3db1bd69e0
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-01-06 16:29:36 +0600

    day 2 conceptual session done

--- index.js
+++ index.js
@@ -75,6 +75,32 @@ async function run() {
       res.send(result);
     })
 
+    //manage user status and role
+    app.patch('/users/:email', verifyToken, async(req, res)=>{
+      const email = req.params.email;
+      const query = {email};
+      const user = await usersCollection.findOne(query);
+      if(!user || user?.status === 'Requested')
+        return res
+                .status(400)
+                .send('You have already requested wait for some time.')
+      
+      const updateDoc = {
+        $set: {
+          status: 'Requested',
+        },
+      }
+      const result = await usersCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    //get user role
+    app.get('/users/role/:email', async(req, res) =>{
+      const email = req.params.email;
+      const result = await usersCollection.findOne({ email });
+      res.send({ role: result?.role});
+    })
+
 
 
     // Generate jwt token
